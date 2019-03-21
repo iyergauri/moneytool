0129f629b3bf87aaa57f2eb0675969b315ac4cb8
iyergauri <iyergauri@gmail.com>
2019-03-20 22:41:40 -0700

    add pretty formatting for before/after

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import pandas as pd
 
 lst = {
-	"Gauri" 	: 14.33,
-	"Megan" 	: 17.88,
-	"Katrina" 	: 16.99,
-	"Izze"		: 15.82
+	"Emily" 	: 13.0,
+	"Karen" 	: 13.0,
+	"Gauri" 	: (21.0/7)+12+5,
+	"Chris" 	: (21.0/7)+13,
+	"Sfen" 		: (21.0/7)+13+4,
+	"Allison" 	: (21.0/7)+13+4,
+	"Vivian" 	: (21.0/7)+15,
+	"Sam" 		: (21.0/7)+15,
+	"Gus"		: (21.0/7)+13+5
 }
 
-total = 80.00
-
-########################################################################
+total = 197
 subtotal = 0
+########################################################################
+print("\n--- LISTED PRICE ---")
+for k, v in lst.items():
+	t = "\t\t" if len(k) < 7 else "\t"
+	print("{}:{}${:.2f}".format(k, t, v))
+
 total *= 100
 for name, cost in lst.items():
 	lst[name] = cost * 100
 	subtotal += (cost * 100)
 
+print("\nsubtotal = {}".format(subtotal/100))
 taxAndTip = total - subtotal
+print("\ntax and tip = {}".format(taxAndTip/100))
 
 if taxAndTip < 0:
 	raise ValueError("Subtotal was greater than total!")
@@ -24,15 +35,14 @@ if taxAndTip < 0:
 for name, cost in lst.items():
 	contrib = int((cost/subtotal) * taxAndTip)
 	lst[name] += contrib
-	print("{} pays ${:.2f} (${:.2f} original + {:.2f}% of the tax, which is ${:.2f})".format(
-		name,
-		lst[name] / 100,
-		cost / 100,
-		(cost/subtotal),
-		contrib/100
-	))
+
 	lst[name] /= 100
 
-# print(lst)
-df = pd.read_json('log.json', lines=True)
-print(df)
+# df = pd.read_json('log.json', lines=True)
+# print(df)
+print("\n--- PROPORTIONAL TAX/TIP ---")
+for k, v in lst.items():
+	t = "\t\t" if len(k) < 7 else "\t"
+	print("{}:{}${:.2f}".format(k, t, v))
+
+print()
